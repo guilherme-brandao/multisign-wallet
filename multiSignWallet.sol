b282207e8fadd681399709cb479cc4c98394dea0
Guilherme Brandão <guilherme1237@gmail.com>
2021-03-08 23:35:41 -0300

    Switching from array storage of the requests to mapping by index

--- multiSignWallet.sol
+++ multiSignWallet.sol
@@ -2,19 +2,19 @@ pragma solidity 0.7.5;
 pragma abicoder v2;
 
 contract MultiSignWallet {
-    
+
     struct Request {
         uint requestId;
         uint amount;
         address payable to;
-        address[] approvals;
+        uint approvals;
         bool done;
     }
     
     uint approvalLimit;
     address[] owners;
-    Request[] requestList;
-    
+    uint[] public requestList;
+     
     modifier onlyOwners() {
         bool owner = false;
         for(uint i=0; i < owners.length;i++) {
@@ -30,8 +30,9 @@ contract MultiSignWallet {
     event ApprovalReceived(uint _id, uint _approvals, address _approver);
     event TransferFinished(uint _id);
 
-    mapping (address => mapping(uint => bool)) ownersApprovals;
+    mapping(address => mapping(uint => bool)) ownersApprovals;
     mapping(address => uint) balance;
+    mapping(uint => Request) public requests;
     
     constructor(address[] memory _owners, uint _limit) {
         approvalLimit = _limit;
@@ -42,25 +43,29 @@ contract MultiSignWallet {
         balance[msg.sender] += msg.value;
     }
     
-    function requestTransfer(uint _amount, address payable _to) public onlyOwners {
+    function requestTransfer(uint _amount, address payable _to) external onlyOwners {
         require(balance[msg.sender] >= _amount, "Balance not sufficient");
-        address[] memory emptyAddressList;
+        
         balance[msg.sender] -= _amount;
+        
         emit RequestCreated(requestList.length, _amount, msg.sender, _to);
-        requestList.push(Request(requestList.length, _amount, _to, emptyAddressList, false));
+        
+        requests[requestList.length] = (Request(requestList.length, _amount, _to, 0, false));
+        
     }
     
-    function approveRequest(uint _indexRequest) public onlyOwners {
+    function approveRequest(uint _indexRequest) external onlyOwners {
         require(ownersApprovals[msg.sender][_indexRequest] == false, "You already approved this request!");
-        require(requestList[_indexRequest].done == false, "The request is already done!");
+        require(requests[_indexRequest].done == false, "The request is already done or were not created yet!");
         
-        Request storage request = requestList[_indexRequest];
-        emit ApprovalReceived(_indexRequest, request.approvals.length, msg.sender);
+        Request memory request = requests[_indexRequest];
         
-        request.approvals.push(msg.sender);
+        emit ApprovalReceived(_indexRequest, request.approvals, msg.sender);
+        
+        request.approvals += 1;
         ownersApprovals[msg.sender][_indexRequest] = true;
         
-        if(request.approvals.length >= approvalLimit) {
+        if(request.approvals >= approvalLimit) {
             request.to.transfer(request.amount);
             request.done = true;
             
@@ -70,12 +75,11 @@ contract MultiSignWallet {
         }
     }
     
-    function getRequests() public view returns (Request[] memory) {
-        return requestList;
+    function getRequest(uint _indexRequest) public view returns (Request memory) {
+        return requests[_indexRequest];
     }
     
     function getBalance() public view returns (uint) {
         return balance[msg.sender];
     }
 }
-   
